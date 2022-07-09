7daf334fac24bb3acc577c2c5439099fc3892fe0
mistermopy <fgullihur@gmail.com>
2022-07-08 19:56:55 -0700

    Added reset button and resize function

--- javascript.js
+++ javascript.js
@@ -1,19 +1,39 @@
 const body = document.querySelector('body');
 const container = document.createElement('div');
+const btnReset = document.querySelector('button');
 body.append(container);
-body.style = 'height: 97vh; width: 97vw;';
-container.style = 'height: 100%; width: 100%; display: flex;';
+body.style = 'height: 90vh; width: 98vw;';
+container.style = 'height: 100%; width: 100%;';
 
-for (let j = 0; j < 16; j++) {
-    let row = document.createElement('div');
-    row.style = 'border: solid black 1px; height: 100%; display: flex; flex: 16; flex-direction: column;';
-    container.append(row);
-    for (let i = 0; i < 16; i++) {
-        let column = document.createElement('div');
-        column.style = 'border: 1px solid black; flex: 16;';
-        column.addEventListener('mouseover', ()=> {
-            column.style.backgroundColor = 'black';
-        });
-        row.append(column);
+function makeGrid(size) {
+    let newGrid = document.createElement('div');
+    newGrid.style = 'height: 100%; width: 100%; display: flex;';
+    newGrid.className = 'newGridClass';
+    for (let j = 0; j < size; j++) {
+        let row = document.createElement('div');
+        row.style = `border: solid black 1px; height: 100%; display: flex; flex: ${size}; flex-direction: column;`;
+        newGrid.append(row);
+        for (let i = 0; i < size; i++) {
+            let column = document.createElement('div');
+            column.style = `border: 1px solid black; flex: ${size};`;
+            column.addEventListener('mouseover', ()=> {
+                column.style.backgroundColor = 'grey';
+            });
+            row.append(column);
+        }
     }
+    container.append(newGrid);
 }
+
+makeGrid(16);
+
+btnReset.addEventListener('click', ()=> {
+    let userSize = prompt('Yargh! How many squares matey?');
+    container.removeChild(document.querySelector('.newGridClass'));
+    if (userSize > 100) {
+        let warning = "sorry! can't go over 100. you should know better...";
+        container.append(warning);
+    } else {
+        makeGrid(userSize);
+    }
+});
